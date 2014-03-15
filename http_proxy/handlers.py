b1b93c1ceaa25c75483db08de3f6ab290afeeaa9
Ivan Korolev <ikorolev@xyzrd.com>
2014-03-16 01:10:31 +0400

    process_asynchronous method

--- http_proxy/handlers.py
+++ http_proxy/handlers.py
@@ -10,6 +10,7 @@ import msgpack
 import logging
 
 from cocaine.services import Service
+from cocaine.asio.engine import asynchronous
 
 request_cnt = 0
 
@@ -24,17 +25,29 @@ class BaseCocaineProxy(RequestHandler):
     def log(self, data):
         logging.info("{0} - {1}".format(self.request_num, data))
 
-    def process_synchronous(self, cocaine_service_name, method, data):
+    def process_synchronous(self, cocaine_service_name, cocaine_method, data):
         """Synchronous Cocaine worker handling."""
         self.log("In process_synchronous()")
         service = Service(cocaine_service_name)
-        response = service.enqueue(
-            method,
-            msgpack.dumps(data)).get()
+        response = service.enqueue(cocaine_method, msgpack.dumps(data)).get()
 
         service.disconnect()
         self.log("process_synchronous() finished")
         return response
+
+    @asynchronous
+    def process_asynchronous(self, cocaine_service_name, cocaine_method, data):
+        """Run selected service and get all chunks as generator."""
+        self.log("In process_asynchronous()")
+        service = Service(cocaine_service_name)
+
+        chunks_g = service.enqueue(cocaine_method, msgpack.dumps(data))
+
+        for chunk in chunks_g:
+            yield umsgpack.loads(chunk)
+
+        service.disconnect()
+        self.log("process_asynchronous() finished")
 
 
 class SleepSynchronous(BaseCocaineProxy):
